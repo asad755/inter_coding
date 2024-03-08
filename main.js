67cac6b6696738515b41e338e0254a972df3c87d
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-09 00:43:55 +0500

    internal/errer
    21

---
+++ main.js
@@ -0,0 +1,8 @@
+// 22.	Intentional Error: If you haven’t received an array index error in one of your programs yet, 
+// try to make one happen. Change an index in one of your programs to produce an index error.
+// Make sure you correct the error before closing the program.
+var different_things = ["Python", "TypeScript", "Rust", "C++", "Pakistan", "Saudia", "Rawalpindi", "Nile", "K2"];
+console.log(different_things[15]);
+console.log(different_things[10]);
+console.log(different_things[8]);
+console.log(different_things[6]);
